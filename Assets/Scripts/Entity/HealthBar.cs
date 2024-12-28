d88d6a04e7d082ab8b09f8dbd1268a4b80a72c83
InSange <ehfrur888@naver.com>
2024-12-28 20:25:41 +0900

    헬스바 기능 추가
    엔티티 체력바 세팅 및 체력 업데이트
    
    #6

---
+++ Assets/Scripts/Entity/HealthBar.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField] private Transform bar;
+    [SerializeField] private Vector3 offset;
+
+    [SerializeField] private float maxHealth;
+    [SerializeField] private Transform target;
+
+    public void SetUp(Transform target, float maxHealth)
+    {
+        this.maxHealth = maxHealth;
+        UpdateBar(maxHealth);
+        this.target = target;
+    }
+
+    public void UpdateBar(float newValue)
+    {
+        float newScale = newValue / maxHealth;
+        Vector3 scale = bar.transform.localScale;
+        scale.x = newScale;
+        bar.transform.localScale = scale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target != null)
+            this.transform.position = target.position + offset;
+    }
+}
